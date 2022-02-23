a41bef0ce9ad95bfc67390de9008d9b47271d273
lxweb102 <lxweb102@gmail.com>
2022-02-23 18:40:46 -0300

    feat: adding support to make deposit to profiles

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,8 +1,41 @@
+const { getMyUnpaidJobs } = require("./job");
+
+/**
+ * 
+ * @param {*} request 
+ * @returns 
+ */
 const getMyBalance = async ( request ) => {
     const balance = request.profile.balance;
     return balance;
 }
 
+const balanceDeposit = async (app, originProfileId, tagetProfileId, amount=0) => {
+    const {Profile} = app.get('models');
+    const originProfile = await Profile.findOne({where: {id: originProfileId }});
+    const targetProfile = await Profile.findOne({where: {id: tagetProfileId }});
+    const unpaidJobs =  await getMyUnpaidJobs(app, originProfileId, 'client');
+    let maxToDeposit = amount;
+    if(unpaidJobs.length){
+        const totalDebt = unpaidJobs.map(a => a.price).reduce((z,x) => z+x);
+        maxToDeposit = totalDebt*0.25;
+    }
+    if(originProfile.balance<maxToDeposit){
+        return {
+            status: 2,
+            error: 'Not enough funds'
+        } 
+    }
+    const balanceDecrement = await originProfile.increment({balance: -maxToDeposit}); // 850
+    const balanceIncrement = await targetProfile.increment({balance: maxToDeposit}); // 551.3
+    return {
+        status: 1,
+        originProfileBalance: originProfile.balance,
+        targetProfileBalance: targetProfile.balance
+    }
+}
+
 module.exports = {
-    getMyBalance
+    getMyBalance,
+    balanceDeposit
 }
--- src/routes/balances.js
+++ src/routes/balances.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {balanceDeposit} = require('../controllers/profile');
+const {getProfile} = require('../middleware/getProfile')
 
 /**
  * @description Deposits money into the the the balance of a client, a client can't deposit more than 25% his total of jobs to pay. (at the deposit moment)
  */
-router.post('/balances/deposit/:userId',  async(req,res)=>{
-    res.json({
-        url:  req.originalUrl
-    })
+router.post('/balances/deposit/:userId', getProfile, async(req,res)=>{
+    if(req.profile.id == req.params.userId) return res.status(404).end('Deposit to your own account are not allowed');
+    const deposit = await balanceDeposit(req.app, req.profile.id, req.params.userId, 100);
+    res.json(deposit)
 })
 
 module.exports = router;
